9850575b3b95d82e98775b5f9cc878037b27d4a2
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2024-06-11 10:29:32 +0000

    new

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """ User model definition """
 
 
-from sqalalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
