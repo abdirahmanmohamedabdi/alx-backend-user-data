b6367a7316b072de4ba857fa8f2f8e7a178bbdec
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-06-13 12:59:02 +0300

    new

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,40 +50,40 @@ class DB:
         for key in kwargs:
             if key not in valid_keys:
                 raise InvalidRequestError
-            query = self._session.query(User)
-            for key in kwargs:
-                if key == 'email':
-                    query = query.filter(
-                                User.email == kwargs[key]
-                                )
-                elif key == 'id':
-                    query = query.filter(
-                                User.id == kwargs[key]
-                                )
-                elif key == 'hashed_password':
-                    query = query.filter(
-                                User.hashed_password == kwargs[key]
-                                )
-                elif key == 'session_id':
-                    query = query.filter(
-                                User.session_id == kwargs[key]
-                                )
-                elif key == 'reset_token':
-                    query = query.filter(
-                                User.reset_token == kwargs[key]
-                                )
-                all_users = query.all()
-                if not all_users:
-                    raise NoResultFound
-                return all_users[0]
+        query = self._session.query(User)
+        for key in kwargs:
+            if key == 'email':
+                query = query.filter(
+                            User.email == kwargs[key]
+                            )
+            elif key == 'id':
+                query = query.filter(
+                            User.id == kwargs[key]
+                            )
+            elif key == 'hashed_password':
+                query = query.filter(
+                            User.hashed_password == kwargs[key]
+                            )
+            elif key == 'session_id':
+                query = query.filter(
+                            User.session_id == kwargs[key]
+                            )
+            elif key == 'reset_token':
+                query = query.filter(
+                            User.reset_token == kwargs[key]
+                            )
+        all_users = query.all()
+        if not all_users:
+            raise NoResultFound
+        return all_users[0]
 
-            def update_user(self, user_id: int, *args, **kwargs) -> None:
-                """ update a user """
-                valid_keys = ['email', 'id', 'hashed_password',
-                              'session_id', 'reset_token']
-                user = self.find_user_by(id=user_id)
-                for key in kwargs:
-                    if key not in valid_keys:
-                        raise ValueError
-                    user.__setattr__(key, kwargs[key])
-                self._session.commit()
+    def update_user(self, user_id: int, *args, **kwargs) -> None:
+        """ update a user """
+        valid_keys = ['email', 'id', 'hashed_password',
+                          'session_id', 'reset_token']
+        user = self.find_user_by(id=user_id)
+        for key in kwargs:
+            if key not in valid_keys:
+                raise ValueError
+            user.__setattr__(key, kwargs[key])
+        self._session.commit()
