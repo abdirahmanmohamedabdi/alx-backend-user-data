7874a35486bd27ba0a2763cf59867d6741f03c0e
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-03 12:44:33 +0300

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def abort_401() -> str:
     """ GET /api/v1/unauthorized
     Return:
@@ -35,7 +35,7 @@ def abort_401() -> str:
     abort(401)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def abort_403() -> str:
     """ GET /api/v1/forbidden
     Return:
