5d50fb91393e69ecd16a018f67c96e3a550dcf4e
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-03 18:26:12 +0300

    new

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,10 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort
 from flask_cors import CORS
 
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,12 +4,13 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
       - 401 status code
-    """
+  """
     abort(401)
 
 
