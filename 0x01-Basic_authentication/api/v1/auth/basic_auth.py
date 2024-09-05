a022b87f96b62b1bfeaafaf50ea66502941e84aa
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-05 12:58:37 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,16 @@ class BasicAuth(Auth):
             return decode_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str) -> (str, str):
+        """Extract user credentials method
+        """	
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
