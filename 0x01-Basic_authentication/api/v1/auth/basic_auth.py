c41ed05e88258b3d21d71aa913a45027672efeb7
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-05 12:50:02 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+         self, base64_authorization_header: str) -> str:
+        """Decode base64 authorization header method
+        """
+        if base64_authorization_header is None:
+            return None
+        if not instance(base64_authorization_header, str):
+            return None
+        try:
+            decode_bytes = base64.b64decode(base64_authorization_header)
+            return decode_bytes.decode('utf-8')
+        except (base64binascii.Error, UnicodeDecodeError):
+            return None
