857b6755353b4fa7c88225f794e03671f2bdd409
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-06 00:32:20 +0300

    new

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Module for session Authentication views
 """
+import os
 from flask import jsonify, request
 from models.user import User
-from api.v1.app import auth
+from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -37,9 +38,18 @@ def handle_login() -> str:
     if users:
         user = users[0]
 
+    if user is None:
+        return jsonify({
+            "error": "no user found for this email"
+        }), 404
     if user.is_valid_password(passwd) is False:
-        session_id = auth.create_session(user.id)
-        cookie_name = os.getenv('SESSION_NAME')
-        resp = jsonify(user.to_json())
-        resp.set_cookie(cookie_name, session_id)
-        return resp
+        return jsonify({
+            "error": "wrong password"
+        }), 401
+
+    from api.v1.app import auth
+    sess_id = auth.create_session(user.id)
+    cookie_name = os.getenv('SESSION_NAME')
+    resp = jsonify(user.to_json())
+    resp.set_cookie(cookie_name, sess_id)
+    return resp
