72b4449f85a3851ae8992854324dd1a523d3e973
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-05 12:51:47 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -27,7 +27,7 @@ class BasicAuth(Auth):
         """
         if base64_authorization_header is None:
             return None
-        if not instance(base64_authorization_header, str):
+        if not isinstance(base64_authorization_header, str):
             return None
         try:
             decode_bytes = base64.b64decode(base64_authorization_header)
