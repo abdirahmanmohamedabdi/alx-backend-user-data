f9f6d72bf812aa23fbc87a6051cab96a63e9c7d1
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-06 00:08:00 +0300

    new

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,7 +60,7 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None and
-       auth.session_cookie(request) is None:
+        auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
