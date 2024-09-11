c01b96200912f36cc391905a914b7c08f2be71f5
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-11 12:10:13 +0300

    new

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,7 +37,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        self._session.add(User)
         self._session.commit()
         return user
 
